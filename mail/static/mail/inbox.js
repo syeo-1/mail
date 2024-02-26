0d0aa4d82816848d98c99af2d291d3e11867a356
syeo-1 <seansqyeo@gmail.com>
2024-02-25 19:07:59 -0500

    send mail functionality

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -5,6 +5,7 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
+  document.querySelector('#compose-form').addEventListener('submit', send_mail)
 
   // By default, load the inbox
   load_mailbox('inbox');
@@ -30,4 +31,28 @@ function load_mailbox(mailbox) {
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+}
+
+function send_mail(event) {
+  // prevent default stops it from submitting a form and bringing you to a new page!!!
+  // refer to 1:09:50 in javascript lecture for something similar since we're doing
+  // client side code!
+  event.preventDefault()
+  console.log('etsetsetset')
+
+  // send an email!
+  fetch('/emails', {
+    method: 'POST',
+    body: JSON.stringify({
+      recipients: document.querySelector('#compose-recipients').value,
+      subject: document.querySelector('#compose-subject').value,
+      body: document.querySelector('#compose-body').value
+    })
+  }).then(response => {
+    response.json();
+  }).then(result => {
+    console.log(result);
+    load_mailbox('sent')
+  });
+
 }
