437299b091a0437e60121a65f243d1965f689f72
syeo-1 <seansqyeo@gmail.com>
2024-02-27 19:59:49 -0500

    can see email body message

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -11,6 +11,10 @@ document.addEventListener('DOMContentLoaded', function() {
   load_mailbox('inbox');
 });
 
+if (!localStorage.getItem('sentMail')) {
+  localStorage.setItem('sentMail', [])
+}
+
 function compose_email() {
 
   // Show compose view and hide other views
@@ -31,14 +35,63 @@ function load_mailbox(mailbox) {
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+
+  console.log(`provided mailbox is: ${mailbox}`)
+  // load the appropriate mailbox
+
+  // do a get request to the appropriate mailbox
+  fetch(`/emails/${mailbox}`)
+  .then(response => response.json()) // if doing arrow function, with return one-liner, remove curly braces!
+  .then(result => {
+    console.log(result);//also looks like need to refresh browser to see results!
+    result.forEach(add_email)
+  });
+
+  // generate divs for the emails and return them!
+  // relevant section in lecture 6: search get new posts and add posts
+
 }
+
+function add_email(contents) {
+  // create a new email
+  const email = document.createElement('div')
+  email.innerHTML = contents.body
+
+  console.log(email)
+
+  // add to DOM
+  document.querySelector('#emails-view').append(email)
+
+}
+
+// [
+//     {
+//         "id": 100,
+//         "sender": "foo@example.com",
+//         "recipients": ["bar@example.com"],
+//         "subject": "Hello!",
+//         "body": "Hello, world!",
+//         "timestamp": "Jan 2 2020, 12:00 AM",
+//         "read": false,
+//         "archived": false
+//     },
+//     {
+//         "id": 95,
+//         "sender": "baz@example.com",
+//         "recipients": ["bar@example.com"],
+//         "subject": "Meeting Tomorrow",
+//         "body": "What time are we meeting?",
+//         "timestamp": "Jan 1 2020, 12:00 AM",
+//         "read": true,
+//         "archived": false
+//     }
+// ]
 
 function send_mail(event) {
   // prevent default stops it from submitting a form and bringing you to a new page!!!
   // refer to 1:09:50 in javascript lecture for something similar since we're doing
   // client side code!
   event.preventDefault()
-  console.log('etsetsetset')
 
   // send an email!
   fetch('/emails', {
