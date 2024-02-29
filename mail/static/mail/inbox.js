2750abb0bfa896c1a3d79b3875a07eed42278fb8
syeo-1 <seansqyeo@gmail.com>
2024-02-29 12:27:34 -0500

    can mark emails as read

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -7,9 +7,31 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#compose').addEventListener('click', compose_email);
   document.querySelector('#compose-form').addEventListener('submit', send_mail)
 
+  document.addEventListener('click', event => {
+    const element = event.target;
+
+    if (element.className === 'open') {
+      element.parentElement.className = 'read email';
+      // set the read field in the actual email in db to true
+      // TODO: how to get id?!?!?!?!?!?!?!?
+      // maybe try using data attributes? have the id as a data attribute on the
+      // button, and when the button is pressed get the value of the data attribute
+      // with the value of the id!
+
+      // get id and do a put request to change the read to true
+      fetch(`/emails/${element.dataset.email_id}`, {
+        method: 'PUT',
+        body: JSON.stringify({
+            read: true
+        })
+      })
+      console.log(element)
+    }
+  })
   // By default, load the inbox
   load_mailbox('inbox');
 });
+
 
 if (!localStorage.getItem('sentMail')) {
   localStorage.setItem('sentMail', [])
@@ -55,9 +77,21 @@ function load_mailbox(mailbox) {
 function add_email(contents) {
   // create a new email
   const email = document.createElement('div')
-  email.innerHTML = contents.body
+  // email.innerHTML = contents.subject
+  console.log(contents.sender)
 
-  console.log(email)
+  // depending on the read/unread state, set the className to either
+  // read email or
+  // unread email
+  if (contents.read === true) {
+    email.className = 'read email'
+  } else {
+    email.className = 'unread email'
+  }
+  email.innerHTML = `from: ${contents.sender} 
+  - ${contents.subject} | ${contents.timestamp} <button data-email_id=${contents.id} class="open">open</button>`
+
+  // console.log(email)
 
   // add to DOM
   document.querySelector('#emails-view').append(email)
